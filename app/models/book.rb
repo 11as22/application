586645d0292e694382cf7e4c8e40b37fa70365ee
11as22 <ec2-user@ip-172-31-3-73.ap-northeast-1.compute.internal>
2023-04-25 07:57:15 +0000

    [Finish]application3

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,18 @@ class Book < ApplicationRecord
   belongs_to :user
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  
+  has_many :favorites,dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  
+  has_one_attached :profile_image
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+  
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+
 end
---
+++ app/models/book_comment.rb
@@ -0,0 +1,6 @@
+class BookComment < ApplicationRecord
+  belongs_to :user
+  belongs_to :book
+
+has_one_attached :profile_image
+end
